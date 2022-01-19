f1d53abcecc93d1610f3f23a48bdc24404c74963
Julian <newspicel11@gmail.com>
2022-01-19 11:57:21 +0100

    Remove Comment

--- .remodel/workspace-export.lua
+++ .remodel/workspace-export.lua
@@ -1,12 +1,7 @@
 local game = remodel.readPlaceFile(".remodel/World.rbxl")
 
--- In this example, we have a bunch of models stored in
--- ReplicatedStorage.Models. We want to put them into a folder named models,
--- maybe for a tool like Rojo.
 local Models = game.Workspace
 
 for _, model in ipairs(Models:GetChildren()) do
 	remodel.writeModelFile(model, "workspace/" .. model.Name .. ".rbxmx")
 end
-
--- And that's it!
