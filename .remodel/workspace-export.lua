afcbe9628cb1044eaa9d5c57d302adcf1d63368a
Julian Haag <me@jhaag.me>
2022-04-26 22:36:11 +0200

    Update workspace-export.lua

--- .remodel/workspace-export.lua
+++ .remodel/workspace-export.lua
@@ -3,6 +3,6 @@ local game = remodel.readPlaceFile(".remodel/place.rbxl")
 local workspace = game.Workspace
 
 for _, content in ipairs(workspace:GetChildren()) do
-	remodel.writeModelFile(content, "workspace/" .. content.Name .. ".rbxmx")
-	print("Exported " .. content.Name .. " to workspace/" .. content.Name .. ".rbxmx")
+	remodel.writeModelFile(content, "workspace/" .. content.Name .. ".rbxm")
+	print("Exported " .. content.Name .. " to workspace/" .. content.Name .. ".rbxm")
 end
